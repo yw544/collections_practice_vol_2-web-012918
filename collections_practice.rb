924d1eddd172da61158fe94ebeeb1077a1afcc11
York Wang <yw544@nyu.edu>
2018-01-23 07:52:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,9 @@
 # your code goes here
 def begins_with_r(arr)
-arr.each.do |element|
-if element[0] != "r" == false
-  return false
-
-end
+arr.each do |element|
+  if element.start_with("r")==false
+    return false
+  end
 end
 true
 end
